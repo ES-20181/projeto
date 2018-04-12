4f7430365816b4fa2ce2d602cadef58cc1977f06
robsonsilv4 <robsonsilv410@gmail.com>
2018-04-12 20:47:19 -0300

    Correção de vendas

--- vendas/models.py
+++ vendas/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class Venda(models.Model):
-    data = models.DateField()
-    descricao = models.CharField(max_lenght=150)
+    data = models.DateField(auto_now=False, auto_now_add=False)
+    descricao = models.CharField(max_length=150)
     valor = models.DecimalField(max_digits=5, decimal_places=2)
     # Enquanto não tem os formulário prontos
-    tipo = models.CharField(max_lenght=15)
+    tipo = models.CharField(max_length=15)
 
-    # def __str__(self):
-    #     return self.data
+    def __str__(self):
+        return str(self.data)
