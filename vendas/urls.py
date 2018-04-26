9c139b8c06cd158c1126db8b3975a34ea357eb7e
robsonsilv4 <robsonsilv410@gmail.com>
2018-04-26 20:28:03 -0300

    Organização e conclusão do CRUD

--- vendas/urls.py
+++ vendas/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import home, venda
+from .views import listar_vendas, nova_venda, atualizar_venda, deletar_venda
 
 urlpatterns = [
-    path('', home),
-    path('venda/<int:codigo>/', venda),
+    path('', listar_vendas, name='listar_vendas'),
+    path('nova', nova_venda, name='nova_venda'),
+    path('atualizar/<int:id>/', atualizar_venda, name='atualizar_venda'),
+    path('deletar/<int:id>/', deletar_venda, name='deletar_venda'),
 ]
--- vendas/views.py
+++ vendas/views.py
@@ -1,15 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Venda
+from .forms import VendaForm
 
-# Create your views here.
 
-
-def home(request):
-    nome = 'Testando'
+def listar_vendas(request):
     vendas = Venda.objects.all()
     return render(request, 'vendas.html', {'vendas': vendas})
 
 
-def venda(request, codigo):
-    venda = Venda.objects.get(id=codigo)
-    return render(request, 'vendas.html', {'venda': venda})
+def nova_venda(request):
+    form = VendaForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('listar_vendas')
+
+    return render(request, 'vendas-form.html', {'form': form})
+
+
+def atualizar_venda(request, id):
+    venda = Venda.objects.get(id=id)
+    form = VendaForm(request.POST or None, instance=venda)
+
+    if form.is_valid():
+        form.save()
+        return redirect('listar_vendas')
+
+    return render(request, 'vendas-form.html', {'form': form, 'venda': venda})
+
+
+def deletar_venda(request, id):
+    venda = Venda.objects.get(id=id)
+
+    if request.method == 'POST':
+        venda.delete()
+        return redirect('listar_vendas')
+
+    return render(request, 'venda-deletar-confirm.html', {'venda': venda})
