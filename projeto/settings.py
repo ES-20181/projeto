c0c79fc0407ec202c1b9c22512f7d75f1474ec45
robsonsilv4 <robsonsilv410@gmail.com>
2018-05-16 19:34:32 -0300

    Adicionado django-bootstrap3 e ajustes no dashboard

--- projeto/settings.py
+++ projeto/settings.py
@@ -32,13 +32,13 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'vendas',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrapform',
 ]
 
 MIDDLEWARE = [
